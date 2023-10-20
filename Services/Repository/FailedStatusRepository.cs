a6b62accaf74c7503dc71671d69d4b4b4f47b78b
Kuc <kucsen2003@gmail.com>
2023-10-20 10:35:43 -0600

    aja

---
+++ Services/Repository/FailedStatusRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+using Services.Generic;
+using Services.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Repository
+{
+    public class FailedStatusRepository : GenericRepository<FailedStatus>, IFailedStatusRepository
+    {
+        public FailedStatusRepository(ErrorQueueContext context) : base(context)
+        {
+        }
+    }
+}
